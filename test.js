668863afe79030793a67ee824f8164972d7ad1e4
researchengineer <144199852+ResearchEngineerer@users.noreply.github.com>
2023-09-06 11:32:48 +0700

    Update test.js

--- test.js
+++ test.js
@@ -1,11 +1,1 @@
 echo 'Ethical hacking';
-var exec = require('child_process').exec;
-
-exec('whoami',
-    function (error, stdout, stderr) {
-        console.log('stdout: ' + stdout);
-        console.log('stderr: ' + stderr);
-        if (error !== null) {
-             console.log('exec error: ' + error);
-        }
-    });
